8ca4ac73647f25a1ef19e9718743be625f12fc18
surono surono <35676155+surono24@users.noreply.github.com>
2019-06-27 17:27:27 +0800

    Add files via upload

---
+++ data/Rumah_sakit_3.js
@@ -0,0 +1,14 @@
+var json_Rumah_sakit_3 = {
+"type": "FeatureCollection",
+"name": "Rumah_sakit_3",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "KODE_UNSUR": "10802", "NAMA_UNSUR": "Rumah Sakit Umum", "TOPONIM": "Palang Merah Indonesia", "PELAKSANA": "PT. Cakrabumi Envirodata", "UPDATED": "20130715", "OBJECTID": "0" }, "geometry": { "type": "Point", "coordinates": [ 124.807104702679155, 1.456825238056446 ] } },
+{ "type": "Feature", "properties": { "KODE_UNSUR": "10802", "NAMA_UNSUR": "Rumah Sakit Umum", "TOPONIM": "Prof. Dr. Ratumbuys", "PELAKSANA": "PT. Cakrabumi Envirodata", "UPDATED": "20130715", "OBJECTID": "0" }, "geometry": { "type": "Point", "coordinates": [ 124.830809279148411, 1.46341193769229 ] } },
+{ "type": "Feature", "properties": { "KODE_UNSUR": "10812", "NAMA_UNSUR": "Rumah Sakit  Bersalin", "TOPONIM": "RSIA Kasih Ibu", "PELAKSANA": "PT. Cakrabumi Envirodata", "UPDATED": "20130715", "OBJECTID": "0" }, "geometry": { "type": "Point", "coordinates": [ 124.822944274722815, 1.461493026916798 ] } },
+{ "type": "Feature", "properties": { "KODE_UNSUR": "10802", "NAMA_UNSUR": "Rumah Sakit Umum", "TOPONIM": "Permata Bunda", "PELAKSANA": "PT. Cakrabumi Envirodata", "UPDATED": "20130715", "OBJECTID": "0" }, "geometry": { "type": "Point", "coordinates": [ 124.856676639001606, 1.481848265728643 ] } },
+{ "type": "Feature", "properties": { "KODE_UNSUR": "10802", "NAMA_UNSUR": "Rumah Sakit Umum", "TOPONIM": "ADVENT", "PELAKSANA": "PT. Cakrabumi Envirodata", "UPDATED": "20130715", "OBJECTID": "0" }, "geometry": { "type": "Point", "coordinates": [ 124.850479733049241, 1.46555870320541 ] } },
+{ "type": "Feature", "properties": { "KODE_UNSUR": "10812", "NAMA_UNSUR": "Rumah Sakit  Bersalin", "TOPONIM": "RSI Siti Maryam", "PELAKSANA": "PT. Cakrabumi Envirodata", "UPDATED": "20130715", "OBJECTID": "0" }, "geometry": { "type": "Point", "coordinates": [ 124.850493849966071, 1.520464223939117 ] } },
+{ "type": "Feature", "properties": { "KODE_UNSUR": "10802", "NAMA_UNSUR": "Rumah Sakit Umum", "TOPONIM": "TNI AL", "PELAKSANA": "PT. Cakrabumi Envirodata", "UPDATED": "20130715", "OBJECTID": "0" }, "geometry": { "type": "Point", "coordinates": [ 124.921954553243779, 1.53542739814445 ] } }
+]
+}
